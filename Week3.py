d8617136aa231c9d70cfd81ed1fa924d08cc0f7c
Anusha Namburu <anushanamburu@Anushas-MacBook-Pro-905.local>
2026-07-07 16:28:09 -0500

    Week 3 Deliverables

---
+++ Week3.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Week 3 Deliverable
+
+# In[19]:
+
+
+import pandas as pd
+
+
+# In[20]:
+
+
+sold = pd.read_csv("ResidentialSoldFINAL.csv")
+list = pd.read_csv("ResidentialListingsFINAL.csv")
+
+
+# ### Fetching Mortgage FRED Dataset
+
+# In[21]:
+
+
+url = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=MORTGAGE30US"
+mortgage = pd.read_csv(url, parse_dates=['observation_date'])
+mortgage.columns = ['date', 'rate_30yr_fixed']
+mortgage
+
+
+# ### Resample Weekly Rates to Monthly Averages
+
+# In[22]:
+
+
+mortgage['year_month'] = mortgage['date'].dt.to_period('M')
+mortgage_monthly = mortgage.groupby('year_month')['rate_30yr_fixed'].mean().reset_index()
+mortgage_monthly
+
+
+# ### Creating Matching ket on MLS Datasets
+
+# In[23]:
+
+
+# Sold dataset — key off CloseDate
+sold['year_month'] = pd.to_datetime(sold['CloseDate']).dt.to_period('M')
+sold['year_month']
+
+
+# In[24]:
+
+
+# Listings dataset — key off ListingContractDate
+list['year_month'] = pd.to_datetime(list['ListingContractDate']).dt.to_period('M')
+list['year_month']
+
+
+# ### Merge
+
+# In[25]:
+
+
+sold_with_rates = sold.merge(mortgage_monthly, on='year_month', how='left')
+sold_with_rates
+
+
+# In[26]:
+
+
+listings_with_rates = list.merge(mortgage_monthly, on='year_month', how='left')
+listings_with_rates
+
+
+# ### Validate Merge
+
+# In[27]:
+
+
+print(sold_with_rates['rate_30yr_fixed'].isnull().sum())
+print(listings_with_rates['rate_30yr_fixed'].isnull().sum())
+
+
+# In[28]:
+
+
+print(
+    sold_with_rates[
+        ['CloseDate', 'year_month', 'ClosePrice', 'rate_30yr_fixed']
+    ].head()
+)
+
